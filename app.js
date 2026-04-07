b4189e4f2d59ca9e2aa60a11712f95228a74a000
deliverygebeta-oss <deliverygebeta@gmail.com>
2026-04-07 13:16:06 +0300

    env.port

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD
 .then(
     () => {
         console.log("****Connected to MongoDB****");
-        app.listen(5000);
+        app.listen(process.env.PORT || 5000);
     }
 ).catch(err => {
     console.log("Failed to connect to MongoDB:", err);
